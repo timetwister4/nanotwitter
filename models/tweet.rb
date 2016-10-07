722a98056176c001278353ac9c8624bc213e76a1
b7q <svin@brandeis.edu>
2016-10-07 16:48:20 -0400

    Added default initalization values

--- models/tweet.rb
+++ models/tweet.rb
@@ -1,6 +1,11 @@
 class Tweet <ActiveRecord::Base
-	belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User"
 
+  after_initialize :set_default_values
 
+  def set_default_values
+    self.likes ||= 0
+    self.reply ||= false
+  end
 
 end
