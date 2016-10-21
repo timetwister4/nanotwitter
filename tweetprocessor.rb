7a5243214bc50aa2d1d45020d89de229fd83c288
Anne Charnes <timetwister4@yahoo.com>
2016-10-21 17:00:07 -0400

    some cleaning, started tweet processing

---
+++ tweetprocessor.rb
@@ -0,0 +1,20 @@
+require 'byebug'
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'models/tweet'
+
+class TweetProcessor
+
+  def make_tweet(text, author_id)
+
+  end
+
+  def make_tags text
+    words=text.split
+    
+
+  end
+
+  def make_mentions text
+
+  end
