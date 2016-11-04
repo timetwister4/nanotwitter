af33fdf857207b051588bb8975a1dc32b2c11d32
Anne Charnes <timetwister4@yahoo.com>
2016-11-04 16:10:47 -0400

    environment.rb modifications and debug statements

--- config/environments.rb
+++ config/environments.rb
@@ -18,6 +18,7 @@ end
 
 configure :production do
   puts "[running in production mode]"
+  puts "*********************************************#{ENV['DATABASE']}"
   ActiveRecord::Base.establish_connection(ENV['DATABASE'])
 end
 
