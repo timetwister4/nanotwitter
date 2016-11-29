d0fc8a57bf50277ff0c8f9d607b73195df50b7b5
Anne Charnes <timetwister4@yahoo.com>
2016-11-29 12:53:39 -0500

    enabled loader.io test

--- app.rb
+++ app.rb
@@ -22,8 +22,8 @@ get '/loaderio-accded2323af55270a8895980c841782.txt' do
   send_file 'loaderio-accded2323af55270a8895980c841782.txt'
 end
 
-get '/loaderio-97e86023c438b5621c512742d95a8419' do
-  "loaderio-97e86023c438b5621c512742d95a8419"
+get '/loaderio-97e86023c438b5621c512742d95a8419.txt' do
+  "loaderio-97e86023c438b5621c512742d95a8419.txt"
 end
 #root
 get '/' do
