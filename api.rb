09457396b62dd5e9c9518601bc3707e76e001d17
Tristanibus <tristan.z.ramirezdeharo@gmail.com>
2016-12-14 12:19:03 -0500

    api improvements

--- api.rb
+++ api.rb
@@ -1,63 +1,87 @@
 require_relative "app.rb"
 
+default = "no_data".to_json
+
 get '/api/v1/tweets/:tweet_id' do
 	if Tweet.where(id: params[:tweet_id].to_i).exists?
-	   @first = Tweet.first.id
-	   @data = Tweet.find(params[:tweet_id].to_i).to_json
-	   erb :test
-	else 
-	   @first = Tweet.first.id
-	   @second = Tweet.second.id
-	   @data = "	no data"
-	   erb :test
+	   Tweet.where(id: params[:tweet_id].to_i).to_json
+	else
+		default
 	end
-
-	
 end
 
-get '/api/v1/tweets/front_feed' do
+get '/api/v1/front-feed' do
    RedisClass.access_ffeed.to_json
 end
 
 get '/api/v1/users/:user_name' do
-  User.find(params[:user_id]).to_json
+  if User.where(user_name: params[:user_name]).exists?
+  	 User.where(user_name: params[:user_name])[0].to_json
+  else
+  	default
+  end
+
 end
 
 get '/api/v1/tweets/:tweet_id/replies' do
-   RedisClass.access_replies(params[:tweet_id]).to_json
+    RedisClass.access_replies(params[:tweet_id]).to_json
 end
 
 get '/api/v1/users/:user_name/tweets' do
-  	User.where(user_name: params[:user_name])[0].tweets.to_json
+   if User.where(user_name: params[:user_name]).exists?
+  	  User.where(user_name: params[:user_name])[0].tweets.to_json
+  else
+  	default
+  end
+  
 end
 
 get '/api/v1/tag/:tag_name' do
-	RedisClass.access_tag(params[:tag_name]).to_json
+	 RedisClass.access_tag(params[:tag_name]).to_json
+
 end
 
 
-get '/api/v1/users/:user_name/home_feed' do
-	u = User.where(:user_name => params[:user_name])
-	RedisClass.access_hfeed(u.id).to_json
+get '/api/v1/users/:user_name/home-feed' do
+	  if User.where(user_name: params[:user_name]).exists?
+	  	  id = User.where(:user_name => params[:user_name])[0].id
+	  	  RedisClass.access_hfeed(id).to_json
+	  else
+	  	default
+	  end
 end
 
 get '/api/v1/follows/:user_name/followings' do
-	u = User.where(:user_name => params[:user_name])
-	Follow.where(follower: u).to_json
+	if User.where(user_name: params[:user_name]).exists?
+	   u = User.where(:user_name => params[:user_name])
+	   Follow.where(follower: u).to_json
+	   
+  	else
+  		default
+  	end
+	
 end
 
 get '/api/v1/follows/:user_name/followers' do
-	u = User.where(:user_name => params[:user_name])
-	Follow.where(followed: u).to_json
+	if User.where(user_name: params[:user_name]).exists?
+	   u = User.where(:user_name => params[:user_name])
+	   Follow.where(followed: u).to_json
+	  
+  	else
+  		default
+  	end
 end
 
 get '/api/v1/tweets/:tweet_id/likes' do
-	RedisClass.access_likes(params[:tweet_id]).to_json
+    RedisClass.access_likes(params[:tweet_id]).to_json
+	
 end
 
 get '/api/v1/tweets/:key_word/search' do
 	TweetFactory = TweetProcessor.new
-	TweetFactory.search_tweets(params[:key_word]).to_json
+  	TweetFactory.search_tweets(params[:key_word]).to_json
+
 end
+
 
 
